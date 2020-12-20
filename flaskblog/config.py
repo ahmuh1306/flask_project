96c150ac9d5c1895c42dd3705f802f30817ed62d
Ahmad Muhammad <info@ahmadmuhammad.co.za>
2020-12-20 11:47:15 +0200

    Added config.json file

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,10 +1,14 @@
 import os
+import json
+
+with open(os.path.abspath('flaskblog/config.json')) as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config['FLASK_SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = config['SQLALCHEMY_DATABASE_URI']
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = '587'
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = config['EMAIL_USER']
+    MAIL_PASSWORD = config['EMAIL_PASS']
